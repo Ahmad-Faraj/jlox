7254a7f3b96eb3a00383b36f66a78d42a51a626e
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2025-02-11 04:06:21 +0200

    Fixed colliding issue when importing Scanner

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
     static boolean hadError = false;
@@ -47,7 +46,7 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        // printing tokens just for now
+        // print tokens
         for (Token token : tokens) {
             System.out.println(token);
         }
