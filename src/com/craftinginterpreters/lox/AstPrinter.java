5c5c5568fbae100c35cc1ad84b89267d640cdcb7
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2025-03-07 06:41:18 +0200

    Interpreter now supports variable creation, reading, and modification! + Mysterious bug fixes (trust me, you’re better off not knowing them 😅)

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -7,8 +7,7 @@ class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
-        return parenthesize(expr.operator.lexeme,
-                expr.left, expr.right);
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
     @Override
@@ -41,12 +40,9 @@ class AstPrinter implements Expr.Visitor<String> {
 
     public static void main(String[] args) {
         Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
+                new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
                 new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Literal(45.67)));
+                new Expr.Grouping(new Expr.Literal(45.67)));
         System.out.println(new AstPrinter().print(expression));
     }
 }
