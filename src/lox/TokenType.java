7d043040e2f4f77377883f169de99c899bbd1b4d
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2025-03-08 13:08:23 +0200

    The guy who replaced you XD ( Now we finished Restructuring 🙃)

---
+++ src/lox/TokenType.java
@@ -0,0 +1,29 @@
+package lox;
+
+enum TokenType {
+    // All of then is public static final becuase of enum
+
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, BREAK, CONTINUE,
+    EOF
+
+    // TODO: Add Contradiction (⊥)
+    // What is a Contradiction (⊥)?
+    // A contradiction occurs when a statement must be both true and false
+    // at the same time, in a way that breaks logic.
+    // we will call it NOX
+}
